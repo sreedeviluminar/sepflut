e7356f831da7dbf9f6b05538a548982890c2dde4
Sreedevi <sreedevi@luminartechnolab.com>
2022-10-20 13:58:58 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,23 @@
 import 'package:flutter/material.dart';
-
-void main() {
+void main(){
   runApp(MaterialApp(
     home: Splash(),
   ));
 }
 
-class Splash extends StatelessWidget {
+class Splash extends  StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Icon(Icons.ac_unit, size: 70, color: Colors.indigo,),
-            Text("SAMPLE APP",
-              style: TextStyle(color: Colors.orangeAccent, fontSize: 30),),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:const [
+              Icon(Icons.adb,size: 80,color: Colors.green,),
+              Text("MYAPP",style: TextStyle(fontSize: 30,color: Colors.red),),
+            ],
+          ),
         ),
-      ),
     );
   }
-
-  l
-
-  ̥
 }
