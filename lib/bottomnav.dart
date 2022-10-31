8b1a4d0a195c7e87f25a61c65a3b821012f7889a
Sreedevi <sreedevi@luminartechnolab.com>
2022-10-31 15:45:45 +0530

    commit

---
+++ lib/bottomnav.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyBottomBar(),
+  ));
+}
+
+class MyBottomBar extends StatefulWidget {
+  @override
+  State<MyBottomBar> createState() => _MyBottomBarState();
+}
+
+class _MyBottomBarState extends State<MyBottomBar> {
+  int selectindex = 0;
+
+  List bodys = const  <Widget>[
+    Card(child: Padding(padding: EdgeInsets.all(50.0), child: Text('Account'),),),
+    Card(child: Padding(padding: EdgeInsets.all(50.0), child: Text('Search'),),),
+    Card(child: Padding(padding: EdgeInsets.all(50.0), child: Text('Favorite'),),),
+    Card(child: Padding(padding: EdgeInsets.all(50.0), child: Text('Home'),),),
+  ];
+
+ void onitemtapped(int index){
+    setState(() {
+      selectindex = index;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomBar'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        unselectedItemColor: Colors.white,
+        selectedItemColor:Colors.yellow ,
+        backgroundColor: Colors.cyan,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle), label: "account",),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), label: "Search",),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: "favorite"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), label: "home")
+        ],
+        currentIndex: selectindex,
+        onTap: onitemtapped,
+      ),
+      body: Center(
+        child: bodys.elementAt(selectindex),
+      ),
+    );
+  }
+}
--- lib/listview_with_separator.dart
+++ lib/listview_with_separator.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MaterialApp(home: ListWithSeparated(),));
-}
-class ListWithSeparated extends   StatelessWidget{
 
-  var datas  = <String>['DATA 1','DATA 2','DATA 3'];
+void main() {
+  runApp(MaterialApp(
+    home: ListWithSeparated(),
+  ));
+}
+
+class ListWithSeparated extends StatelessWidget {
+  var datas = <String>['DATA 1', 'DATA 2', 'DATA 3'];
   var colors = <int>[600, 300, 100];
-  var sepcol = [800,400,200];
+  var sepcolors = <int>[600, 300, 100];
 
   @override
   Widget build(BuildContext context) {
-        return Scaffold(
-          body: ListView.separated(
-              itemBuilder: (context, index){
-                return Card(
-                  color:Colors.green[colors[index]],
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("MyList"),
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) {
+              return Card(
+                color: Colors.pink[colors[index]],
+                child: Padding(
+                  padding: const EdgeInsets.all(50.0),
                   child: Text(datas[index]),
-                );
-              },
-              separatorBuilder: (context, index){
-                return Icon(Icons.ac_unit,color: Colors.red[sepcol[index]],);
-              },
-              itemCount: datas.length),
-        );
-    }
-
+                ),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return Icon(
+                Icons.account_tree,
+                color: Colors.green[sepcolors[index]],
+              );
+            },
+            itemCount: datas.length));
+  }
 }
